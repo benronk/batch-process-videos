7dbab28879a2b9c53ee7eaac41f31d2c064d090a
Benjamin Ronk <ben@rvshare.com>
2019-06-11 15:27:30 -0400

    Process based on filename rather than folder.
    Before I would put files to process in a processme folder. Plex would get confused and would try to match the movie/show called processme. Now I'll put the file in the correct place, named correctly, and when i want it processed i'll use Automator to batch rename them and add processme before the extensions. processme720 should convert down to 720 instead of 1080.

--- process.rb
+++ process.rb
@@ -5,20 +5,21 @@ require 'fileutils'
 require 'pp'
 
 @EXCLUDED_DIRS = ['/deal.with.these', '/raw', '/woodworking', '/paw patrol' '/wild.kratts', '/word world']
-@PROCESS_FILES_FOLDER = '/processme'
+@PROCESS_FILES_INCLUDING_THIS = ['processme', 'processme720']
 @VIDEO_EXTENSIONS = [".mkv", ".mp4", ".m4v", ".divx", ".mpg"]
 
 @logger = Logger.new('logs/process.log', 'daily')
 @total_space_reduction = 0
 
-@paths = ['/Volumes/storage/videos/tv.shows/',
-         '/Volumes/storage/videos/movies']
+@paths = ['/Users/bronk/dev/batch-process-videos/test_files',
+          '/Volumes/storage/videos/tv.shows/',
+          '/Volumes/storage/videos/movies']
 
 def get_files (path)
   Dir[ File.join(path, '**', '*') ]
     .reject{ |f| File.directory? f}
-    .reject{ |f| @EXCLUDED_DIRS.any? { |p| File.dirname(f).downcase.include? p}}
-    .select{ |f| File.dirname(f).downcase.include? @PROCESS_FILES_FOLDER}
+    .reject{ |f| @EXCLUDED_DIRS.any? { |p| File.dirname(f).downcase.include? p.downcase}}
+    .select{ |f| @PROCESS_FILES_INCLUDING_THIS.any? { |p| File.basename(f).downcase.include? p.downcase}}
 end
 
 def process_files(files)
@@ -29,9 +30,7 @@ def process_files(files)
     if File.file?(file) && @VIDEO_EXTENSIONS.include?(File.extname(file))
       transcode_file(file)
     else
-      move_here = File.dirname(file).sub!(@PROCESS_FILES_FOLDER, '')
-      @logger.info "skipping: #{file} and moving to #{move_here}"
-      FileUtils.mv file, move_here
+      @logger.info "skipping #{file} because file extension says it's not a video"
     end
 
     i = i - 1
@@ -40,33 +39,36 @@ def process_files(files)
 end
 
 def transcode_file(file)
-  @logger.info "transcode start: #{File.basename(file)}"
+  @logger.info "transcode start: #{File.basename(file)}" 
+
   start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 
-  transcode_file = file.sub(@PROCESS_FILES_FOLDER, '')
-  transcode_folder = File.dirname(transcode_file)
-  deleteme_file = transcode_file.sub('videos', 'videos/processed')
+  process_tag = @PROCESS_FILES_INCLUDING_THIS.select { |n| File.basename(file).downcase.include? '.'+n.downcase+'.'}
+
+  transcoded_file = file.sub(process_tag, '')
+  deleteme_file = transcoded_file.sub('videos', 'videos/processed')
   deleteme_folder = File.dirname(deleteme_file)
-  
-  if !Dir.exist? transcode_folder
-    @logger.info "creating: #{transcode_folder}"
-    FileUtils.mkdir_p transcode_folder
-  end
 
   # if this file is already a file in the processed folder it's probably incomplete so delete it so we can transcode it again
-  if File.exist? transcode_file
-    File.delete transcode_file
+  if File.exist? transcoded_file
+    File.delete transcoded_file
+  end
+  
+  if process_tag.include? '720'
+    # %x(transcode-video --no-log --encoder vt_h264 --720 --target small --output "#{transcoded_file}" "#{file}")
+    %x(transcode-video --no-log --720 --target small --output "#{transcoded_file}" "#{file}")
+  else
+    # %x(transcode-video --no-log --encoder vt_h264 --target small --output "#{transcoded_file}" "#{file}")
+    %x(transcode-video --no-log --target small --output "#{transcoded_file}" "#{file}")
   end
 
-  %x(transcode-video --no-log --encoder vt_h264 --target small --output "#{transcode_file}" "#{file}")
-
-  if !File.exist? transcode_file
+  if !File.exist? transcoded_file
     @logger.info "failed transcode for some reason: #{file}"
-    @logger.info "run this command to find out why it failed:: transcode-video -vv --no-log --encoder vt_h264 --target small --output #{transcode_file} #{file}"
+    @logger.info "run this command to find out why it failed:: transcode-video -vv --no-log --encoder vt_h264 --target small --output #{transcoded_file} #{file}"
   else
     finish = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    @total_space_reduction += Filesize.from(File.size(file).to_s + " b") - Filesize.from(File.size(transcode_file).to_s + " b")
-    @logger.info "transcode finished - time: #{((finish - start)/60).ceil} minutes - smaller by: #{(Filesize.from(File.size(file).to_s + " b") - Filesize.from(File.size(transcode_file).to_s + " b")).pretty} - total space reduced: #{@total_space_reduction.pretty}"
+    @total_space_reduction += Filesize.from(File.size(file).to_s + " b") - Filesize.from(File.size(transcoded_file).to_s + " b")
+    @logger.info "transcode finished - time: #{((finish - start)/60).ceil} minutes - smaller by: #{(Filesize.from(File.size(file).to_s + " b") - Filesize.from(File.size(transcoded_file).to_s + " b")).pretty} - total space reduced: #{@total_space_reduction.pretty}"
 
 
     if !Dir.exist? deleteme_folder
@@ -84,6 +86,7 @@ end
 
 @paths.each do |path|
   @logger.info "START path: #{path}"
+  p "#{path}"
   if Dir.exist?(path)
     files = get_files(path)
     @logger.info "#{files.count} files to process"
